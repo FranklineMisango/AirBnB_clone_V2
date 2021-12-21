f5410f1f7c33177c6ca9138eaed8ad982529f3d9
Virgo Alpha <bensonkingori96@gmail.com>
2021-12-21 11:47:29 +0000

    d

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if s inside
+        if obj is equal to None, the method should not do anything"""
+
+        if obj:
+            del(self.__objects["{}.{}".format(type(obj).__name__, obj.id)])
+            del(obj)it
