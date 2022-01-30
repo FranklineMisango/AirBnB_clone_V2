a84cd3e37590972e7a503b235e2714b0df5bda85
Virgo Alpha <bensonkingori96@gmail.com>
2022-01-30 14:40:04 +0400

    s

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 A script that starts a Flask Web Application
+/: Diplays "Hello HBNB!"
+/hbnb: Displays "HBNB"
 """
 
 
@@ -11,6 +13,10 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
 
 
 if __name__ == "__main__":
